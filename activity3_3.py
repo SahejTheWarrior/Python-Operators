89e6c4a6c8fc276daa2a4ac10a7ae3594f44cf52
SahejTheWarrior <sahejburande@gmail.com>
2025-08-26 12:01:39 +0100

    Add files via upload

---
+++ activity3_3.py
@@ -0,0 +1,11 @@
+print("enter the marks obtained ion 4 subjets")
+math=int(input("math :"))
+english=int(input("english :"))
+science=int(input("science :"))
+art=int(input("art :"))
+
+sum=math+english+science+art
+print("the sum of all subjects :",sum)
+perc=(sum/400)*100
+print(end="perc mark  =")
+print(perc)
